efe7478982db8f3a180f2f23798a981e5b561eaa
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2020-04-23 22:44:54 +0200

    add isNullOrEmpty for String

--- lib/src/string.dart
+++ lib/src/string.dart
@@ -137,4 +137,14 @@ extension StringX on String {
   /// print('message digest'.md5); //f96b697d7cb7938d525a2f31aaf161d0
   /// ```
   String get md5 => crypto.md5.convert(toUtf8()).toString();
+  
+  bool get isNullOrEmpty {
+    if (this == null) {
+      return true;
+    }
+
+    return this.isEmpty;
+  }
+
+  bool get isNotNullOrEmpty => !this.isNullOrEmpty;
 }
