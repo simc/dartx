652fd9be0404fa066a9500372513600acf127c6b
Pascal Welsch <pascal@welsch.dev>
2021-03-14 16:34:48 +0100

    Own List extensions

--- lib/src/list.dart
+++ lib/src/list.dart
@@ -2,7 +2,7 @@
 
 part of dartx;
 
-extension ListX<E> on List<E> {
+extension ListExtension<E> on List<E> {
   /// Index of the first element or -1 if the collection is empty.
   ///
   /// ```dart
@@ -11,7 +11,9 @@ extension ListX<E> on List<E> {
   /// [].firstIndex; // -1
   /// ```
   int get firstIndex => isNotEmpty ? 0 : -1;
+}
 
+extension ListLastIndexExtension<E> on List<E> {
   /// Index of the last element or -1 if the collection is empty.
   ///
   /// ```dart
@@ -20,14 +22,18 @@ extension ListX<E> on List<E> {
   /// [].lastIndex; // -1
   /// ```
   int get lastIndex => length - 1;
+}
 
+extension ListIndicesExtension<E> on List<E> {
   Iterable<int> get indices sync* {
     var index = 0;
     while (index <= lastIndex) {
       yield index++;
     }
   }
+}
 
+extension ListDropExtension<E> on List<E> {
   /// Returns a new list containing all elements except first [n] elements.
   List<E> drop(int n) {
     if (n < 0) {
@@ -40,7 +46,9 @@ extension ListX<E> on List<E> {
     if (resultSize == 1) return [last!];
     return sublist(n);
   }
+}
 
+extension ListDropWhileExtension<E> on List<E> {
   /// Returns a new list containing all elements except last elements that
   /// satisfy the given [predicate].
   List<E> dropWhile(bool Function(E element) predicate) {
@@ -54,7 +62,9 @@ extension ListX<E> on List<E> {
     if (startIndex == null) return [];
     return sublist(startIndex);
   }
+}
 
+extension ListDropLastExtension<E> on List<E> {
   /// Returns a new list containing all elements except last [n] elements.
   List<E> dropLast(int n) {
     if (n < 0) {
@@ -67,7 +77,9 @@ extension ListX<E> on List<E> {
     if (resultSize == 1) return [first];
     return sublist(0, length - n);
   }
+}
 
+extension ListDropLastWhileExtension<E> on List<E> {
   /// Returns a new list containing all elements except last elements that
   /// satisfy the given [predicate].
   List<E> dropLastWhile(bool Function(E element) predicate) {
@@ -81,7 +93,9 @@ extension ListX<E> on List<E> {
     if (endIndex == null) return [];
     return sublist(0, endIndex + 1);
   }
+}
 
+extension ListLowerBoundExtension<E> on List<E> {
   /// Returns the first position in this list that does not compare less than
   /// [value].
   ///
@@ -96,7 +110,9 @@ extension ListX<E> on List<E> {
   int lowerBound(E value, {int Function(E a, E b)? compare}) {
     return collection.lowerBound(this, value, compare: compare);
   }
+}
 
+extension ListBinarySearchExtension<E> on List<E> {
   /// Returns a position of the [value] in this list, if it is there.
   ///
   /// If the list isn't sorted according to the [compare] function, the result
@@ -110,7 +126,9 @@ extension ListX<E> on List<E> {
   int binarySearch(E value, {int Function(E a, E b)? compare}) {
     return collection.binarySearch(this, value, compare: compare);
   }
+}
 
+extension ListInsertionSortExtension<E> on List<E> {
   /// Sort this list between [start] (inclusive) and [end] (exclusive) using
   /// insertion sort.
   ///
@@ -130,7 +148,9 @@ extension ListX<E> on List<E> {
   void insertionSort({Comparator<E>? comparator, int start = 0, int? end}) {
     collection.insertionSort(this, compare: comparator, start: start, end: end);
   }
+}
 
+extension ListMergeSortExtension<E> on List<E> {
   /// Sorts this list between [start] (inclusive) and [end] (exclusive) using
   /// the merge sort algorithm.
   ///
@@ -150,7 +170,9 @@ extension ListX<E> on List<E> {
   void mergeSort({int start = 0, int? end, Comparator<E>? comparator}) {
     collection.mergeSort(this, start: start, end: end, compare: comparator);
   }
+}
 
+extension ListSwapExtension<E> on List<E> {
   /// Swaps the elements in the indices provided.
   ///
   /// ```dart
@@ -164,7 +186,7 @@ extension ListX<E> on List<E> {
   }
 }
 
-extension ListListX<E> on List<List<E>> {
+extension ListFlattenExtension<E> on List<List<E>> {
   /// Returns a new [List] of all elements from all lists in this
   /// [List].
   ///
