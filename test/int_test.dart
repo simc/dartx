d32cae77355dc8a93c8f67c2dc0ada42bb4c02e0
Biplab Dutta <biplabdutta860@gmail.com>
2022-03-04 23:29:32 +0545

    test: add some more test case

--- test/int_test.dart
+++ test/int_test.dart
@@ -14,6 +14,12 @@ void main() {
           expect(3.ordinal(), '3rd');
           expect(4.ordinal(), '4th');
           expect(10.ordinal(), '10th');
+          expect(101.ordinal(), '101st');
+          expect(102.ordinal(), '102nd');
+          expect(103.ordinal(), '103rd');
+          expect(111.ordinal(), '111th');
+          expect(112.ordinal(), '112th');
+          expect(113.ordinal(), '113th');
         },
       );
     },
